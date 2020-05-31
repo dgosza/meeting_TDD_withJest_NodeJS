802e0449fa67be3c1ec600b4e037c33fa4c3b4df
dieezs <diegomagasouza@gmail.com>
2020-05-30 23:43:33 -0300

    meeting tdd with jest

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs')
+const {User} = require('../../src/app/models')
+
+const truncate = require('../utils/truncate')
+
+describe('User', () => {
+    
+    beforeEach(async () => {
+        await truncate();
+    })
+
+
+    it('should encrypt user password', async() => {
+        
+        const user = await User.create({
+            name: 'Diego',
+            email: 'diegomagasouza@gmail.com',
+            password: '123456'
+        })
+
+        const compare = await bcrypt.compare('123456', user.password_hash)
+
+        expect(compare).toBe(true)
+
+    })
+
+
+})
